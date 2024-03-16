7c86387a4710b05f38867025d7c40142d617ca72
MadLongTom <36219016+MadLongTom@users.noreply.github.com>
2024-03-16 14:35:35 +0800

    update(desc)

---
+++ Models/TiJiaoRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prolegomenon.Models
+{
+    internal class TiJiaoRoot
+    {
+        public string Sum { get; set; }
+        public string HeGe { get; set; }
+    }
+}
